f6390c6d730a81f5ffccd65f474a0003cb403675
Renjith Ravindranathan <mysticrenji@users.noreply.github.com>
2023-05-26 16:08:21 +0200

    Update configure-devopsbox.sh

--- configure-devopsbox.sh
+++ configure-devopsbox.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 sudo apt-get update
 sudo apt-get install -y python3-dev python3-pip
-pip3 install setuptools-rust pywinrm
+pip3 install setuptools-rust pywinrm ansible
 pip3 install --upgrade pip
-sudo apt install -y ansible
-ansible-galaxy collection install ansible.windows
+ansible-galaxy collection install community.windows
+
+sudo apt-get update && sudo apt-get install -y gnupg software-properties-common gpg
+wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update && sudo apt install -y terraform
